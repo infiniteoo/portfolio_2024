817f36aadaa498892a83771d68ea0a4ee22fe947
Troy Dorman <troydorman@gmail.com>
2023-09-20 18:05:51 -0600

    tilt integrated and updated mock images

--- src/components/portfolio/PortfolioBlock.js
+++ src/components/portfolio/PortfolioBlock.js
@@ -1,24 +1,46 @@
-import React from 'react';
-import IconLink from "./IconLink";
-import {Box} from "@mui/material";
+import React from 'react'
+import IconLink from './IconLink'
+import { Box } from '@mui/material'
+import { Tilt } from 'react-tilt'
 
 function PortfolioBlock(props) {
-   const {image, live, source, title} = props;
-   return (
-      <Box display={'flex'} flexDirection={'column'} justifyContent={'center'} alignItems={'center'}>
-         <Box component={'img'} src={image} alt={'mockup'}/>
-         <h1 style={{fontSize: '2rem'}}>{title}</h1>
-         <Box className={'portfolio'} display={'flex'} flexDirection={'column'} gap={'0.5rem'}
-              alignItems={'center'} fontSize={'1.5rem'} py={'2rem'}>
-            <Box p={1} border={'2px solid black'} borderRadius={'25px'}>
-               <IconLink link={live} title={'Live Demo'} icon={'fa fa-safari'}/>
-            </Box>
-            <Box p={1} border={'2px solid black'} borderRadius={'25px'}>
-               <IconLink link={source} title={'Source Code'} icon={'fa fa-code'}/>
-            </Box>
-         </Box>
+  const { image, live, source, title } = props
+  return (
+    <Box
+      display={'flex'}
+      flexDirection={'column'}
+      justifyContent={'center'}
+      alignItems={'center'}
+    >
+      <Tilt
+        options={{
+          max: 15,
+          scale: 1,
+          speed: 450,
+        }}
+        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
+      >
+        <Box component={'img'} src={image} alt={'mockup'} />
+      </Tilt>
+      <h1 style={{ fontSize: '2rem' }}>{title}</h1>
+      <Box
+        className={'portfolio'}
+        display={'flex'}
+        flexDirection={'column'}
+        gap={'0.5rem'}
+        alignItems={'center'}
+        fontSize={'1.5rem'}
+        py={'2rem'}
+      >
+        <Box p={1} border={'2px solid black'} borderRadius={'25px'}>
+          <IconLink link={live} title={'Live Demo'} icon={'fa fa-safari'} />
+        </Box>
+        <Box p={1} border={'2px solid black'} borderRadius={'25px'}>
+          <IconLink link={source} title={'Source Code'} icon={'fa fa-code'} />
+        </Box>
       </Box>
-   );
+    </Box>
+  )
 }
 
-export default PortfolioBlock;
+export default PortfolioBlock
